941573bb65afb3c4bcb8593e5ad8b3ada0d20196
Charles Pletcher <cpletcher2@gmail.com>
2016-05-05 13:04:17 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,28 @@
+var animal = 'dog'
+
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
+
+function add2(n) {
+  return n + two
+
+  // Feel free to move things around!
+  const two = 2
+}
+
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+// We want this to return "FUNKY!" -- how can we accomplish that?
+var theFunk = funkyFunction()
---
+++ test/index-test.js
@@ -0,0 +1,53 @@
+/*global describe, it */
+
+const expect = require('chai').expect
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
+
+const file = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+
+describe('index', () => {
+  jsdom({
+    src: file
+  })
+
+  describe('myAnimal()', () => {
+    it('returns my animal', () => {
+      expect(myAnimal()).to.equal('dog')
+    })
+  })
+
+  describe('yourAnimal()', () => {
+    it('returns your animal', () => {
+      expect(yourAnimal()).to.equal('cat')
+    })
+
+    it('does not hard-code the answer', () => {
+      expect(yourAnimal.toString()).not.to.contain("return 'cat'")
+    })
+  })
+
+  describe('add2(n)', () => {
+    it('adds two to n', () => {
+      const n = Math.floor(Math.random() * 1000)
+      expect(add2(n)).to.equal(n + 2)
+    })
+  })
+
+  describe('funkyFunction()', () => {
+    it('returns a function', () => {
+      expect(funkyFunction()).to.be.a('function')
+    })
+  })
+
+  describe('theFunk', () => {
+    it('is "FUNKY!"', () => {
+      expect(theFunk).to.equal('FUNKY!')
+    })
+
+    it('does not hard-code the answer', () => {
+      expect(file).to.contain('var theFunk = funkyFunction()')
+    })
+  })
+})
