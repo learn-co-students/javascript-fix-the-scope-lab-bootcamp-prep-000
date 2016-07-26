6b6769d6ce0db78569c328354abeed6e8ebed7b1
Charles Pletcher <cpletcher2@gmail.com>
2016-07-26 15:19:59 -0400

    Update instructions

--- index.js
+++ index.js
@@ -24,5 +24,5 @@ var funkyFunction = function() {
   }
 }
 
-// We want this to return "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction()
+// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
+var theFunk = funkyFunction
