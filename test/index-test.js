691ae8f4c61e3c123e8bec076664565a43feab36
Charles Pletcher <cpletcher2@gmail.com>
2016-07-29 12:06:35 -0400

    Fix tests

--- test/index-test.js
+++ test/index-test.js
@@ -1,44 +1,39 @@
 /*global describe, it */
 
-const fs = require('fs');
-const path = require('path');
-const file = fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8');
-
-describe('myAnimal()', function() {
-  it('returns my animal', () => {
-    expect(myAnimal()).toEqual('dog')
-  })
-})
-
-describe('yourAnimal()', function() {
-  it('returns your animal', function(){
-    expect(yourAnimal()).toEqual('cat')
+describe('Fix the Scope', function() {
+  describe('myAnimal()', function() {
+    it('returns my animal', () => {
+      expect(myAnimal()).toEqual('dog')
+    })
   })
 
-  it('does not hard-code the answer', function() {
-    expect(yourAnimal.toString()).toNotContain("return 'cat'")
-  })
-})
+  describe('yourAnimal()', function() {
+    it('returns your animal', function(){
+      expect(yourAnimal()).toEqual('cat')
+    })
 
-describe('add2(n)', function() {
-  it('adds two to n', function() {
-    const n = Math.floor(Math.random() * 1000)
-    expect(add2(n)).toEqual(n + 2)
-  })
-})
-
-describe('funkyFunction()', function() {
-  it('returns a function', function() {
-    expect(typeof funkyFunction()).toEqual('function')
-  })
-})
-
-describe('theFunk', function() {
-  it('is "FUNKY!"', function() {
-    expect(theFunk).toEqual('FUNKY!')
+    it('does not hard-code the answer', function() {
+      expect(yourAnimal.toString()).toNotContain("return 'cat'")
+    })
   })
 
-  it('does not hard-code the answer', function() {
-    expect(file).toContain('var theFunk = funkyFunction()')
+  describe('add2(n)', function() {
+    it('adds two to n', function() {
+      const n = Math.floor(Math.random() * 1000)
+      expect(add2(n)).toEqual(n + 2)
+    })
   })
+
+  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(theFunk).toEqual('FUNKY!')
+    })
+  })
+
 })
