17a433f4a228c5339cad26171234a5197abdcf91
pletcher <charles.pletcher@gmail.com>
2016-08-05 16:14:27 -0400

    Patch up root.js

--- test/root.js
+++ test/root.js
@@ -10,15 +10,17 @@ before(function(done) {
     presets: ['es2015']
   });
 
-
   const html = path.resolve(__dirname, '..', 'index.html')
 
   jsdom.env(html, [], {
     src: babelResult.code,
     virtualConsole: jsdom.createVirtualConsole().sendTo(console)
   }, (err, window) => {
-    global.window = window
+    if (err) {
+      return done(err);
+    }
 
+    global.window = window;
 
     return done(err);
   });
