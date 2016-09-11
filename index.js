d69c25314398f5a52333d0153ea2a0f731d3c4be
Andrew W <kairospirate@gmail.com>
2016-09-11 23:26:11 +0000

    Done.

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ function myAnimal() {
   return animal
 }
 
+
 function yourAnimal() {
+  var animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,17 +14,21 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
+
   }
 }
 
+var theFunk = funkyFunction()()
+
+
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
