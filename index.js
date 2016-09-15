79bbfe08b811af3b1073c5bd0f7a2945b2046526
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-15 22:10:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 var animal = 'dog'
 
 function myAnimal() {
+  var anmial ='cat'
   return animal
 }
 
 function yourAnimal() {
+  var animal='cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,17 +14,51 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
+  var add2 = n + 2
   const two = 2
-}
+  return n + two
+} // Feel free to move things around!
+
+// 4 things:
+// Setting variables
+// var name = "Neil"
+// var weight = 145
+// var cart = {}
+
+// Getting variables
+// console.log(name)
+// alert(soupFlavor)
+
+// Defining Functions (Javascript Closures)
+// function(){
+// }
+// function stupid(){
+// }
+// function multiply(){
+//   return a*b;
+// }
+// function alertString(s){
+//   alert(s);
+// }
+// function logString(s){
+//   console.log(s);
+// }
+
+// Executing Functions
+// x = multiply(4,5) //x => 20
+// y = stupid() //y=> undefined
+// alertString("steve")
+// z = logString("Lunch") // z=>
+
+//(Instantiate Objects)
+// Array.new
+// Object.new
 
 var funkyFunction = function() {
-  return function() {
-    return "FUNKY!"
+  return function() {return "FUNKY!"
   }
-}
+  }
+
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
