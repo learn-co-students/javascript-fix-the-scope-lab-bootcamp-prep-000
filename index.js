52140ac70c8664894ff659bcad0f622b50089f18
Yelstin Fernandes <yfernan2@u.rochester.edu>
2016-10-26 17:36:28 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var animal = 'dog'
+var animal = 'dog'                          //global variable of animal that contains dog
 
 function myAnimal() {
   return animal
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'                        //create a local variable named animal that gets called in yourAnimal which contains cat
   return animal
 }
 
 function add2(n) {
+  const two = 2                             //never learned what const is but assuming it is a constant variable? the issue with this was that it was defined after return (in which it calls two), so I moved it to before
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
@@ -25,4 +23,4 @@ var funkyFunction = function() {
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()             //added ()() so you have access to the string buried under 2 nested functions in funkyFunction
