acc9afee512cf442ebec0c9d22f3e95d1c131604
Katerina  Papacostas <katpapacostas@gmail.com>
2016-11-03 20:29:37 +0000

    Done.

--- index.js
+++ index.js
@@ -19,12 +19,10 @@ function add2(n) {
 // Feel free to move things around!
 }
 
-
-function funkyFunction() {
-  return function() {
-    return "FUNKY!"
-  }
+// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
+var funkyFunction = function() {
+  return function(){ return "FUNKY!" }
 }
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction()
+
+var theFunk = funkyFunction()()
