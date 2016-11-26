8e3f477af8f052f9c3188177ac45b626f4557873
Talise Hendrix <spasibo09@gmail.com>
2016-11-26 15:36:55 +0000

    Done.

--- index.js
+++ index.js
@@ -3,26 +3,29 @@ var animal = 'dog'
 function myAnimal() {
   return animal
 }
+myAnimal()
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = "cat"
   return animal
 }
-
-function add2(n) {
+const two = 2 // I had to move constant out side of function. googled const. const is somewhat a cemented value that sort
+function add2(n) {   // (from above) that sort of cant be changed . no reassignments allowed. more or lesss.
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
-  return function() {
+  return  function() {  //Shouldnt i name the function to make it exist? and then run it outside it. but in bigger function?
     return "FUNKY!"
   }
+
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+var theFunk = funkyFunction()() // apprently to run more functions you add another paren.
