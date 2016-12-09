35ad4d8bc1d2ff105e863a999925fa6d71b446ed
rotimi99 <timea.roik@gmail.com>
2016-12-08 17:21:25 -0800

    Update index.js

--- index.js
+++ index.js
@@ -15,9 +15,9 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  var theFunk = funkyFunction() {
-    return function() {
-      return "FUNKY!"
-    }
+  return function() {
+    return "FUNKY!" 
   }
 }
+
+var theFunk = funkyFunction()();
