c83c1ba04af8a2224aaa3387e12c52ea2855f9e0
Steven Hui <stevenhhui@gmail.com>
2016-12-28 20:33:03 +0000

    Done.

--- index.js
+++ index.js
@@ -8,15 +8,22 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  // Added (var animal = 'cat') inside the function yourAnimal()
+  // and called youAnimal(); afterwards
+  var animal = 'cat';
   return animal
 }
+yourAnimal();
 
+// Moved 'const two = 2 above 'return n + 2' because 'return' ends all code
+// following it afterwards
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
+
 
 var funkyFunction = function() {
   return function() {
@@ -25,4 +32,6 @@ var funkyFunction = function() {
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction
+// Adding '()' calls/excutes a function. Adding another '()' afterwards calls a function
+// within a function, since var funkyFunction = a function that returns another function
+var theFunk = funkyFunction()()
