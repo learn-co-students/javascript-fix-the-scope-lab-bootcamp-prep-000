436617acd336c9a5c14af618ba18d8d72c07e13b
Caroline Wynspeare <carolinewynspeare@gmail.com>
2017-01-13 01:47:23 +0000

    Done.

--- index.js
+++ index.js
@@ -23,8 +23,8 @@ const two = 2
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
-  }
-}
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = function()
+  }
+
+}
+var theFunk = funkyFunction()()
