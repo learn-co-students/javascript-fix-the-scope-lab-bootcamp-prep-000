026d8a3d9d6530cbb2aa95d1f487354920342126
Hima Chitalia <hima_chhag@yahoo.com>
2017-01-20 19:36:58 +0000

    Done.

--- index.js
+++ index.js
@@ -20,9 +20,11 @@ function add2(n) {
 var funkyFunction = function()
 {
 return function() {
+  theFunk = "FUNKY!"
   return "FUNKY!"
 }
 }
 
 // We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction()();
+var theFunk = funkyFunction();
+theFunk();
