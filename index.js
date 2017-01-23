f3498ed467fc71c953914051e2b7341c98bbc036
Graham Squires <graham.squires3@gmail.com>
2017-01-23 15:08:31 +0000

    Done.

--- index.js
+++ index.js
@@ -1,14 +1,15 @@
 var animal = 'dog'
-
+var innerVar ='cat'
 function myAnimal() {
   return animal
 }
 
 function yourAnimal() {
+  return innerVar
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+
 }
 
 function add2(n) {
