cc9f3e455511a2000dc5e35419837a1229d91cc4
Christofer Herlin <christofer.herlin@gmail.com>
2017-01-24 15:07:59 +0000

    Done.

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  // Feel free to move things around!
   const two = 2
   return n + two
 }
@@ -21,5 +20,4 @@ var funkyFunction = function() {
   }
 }
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
 var theFunk = funkyFunction()()
