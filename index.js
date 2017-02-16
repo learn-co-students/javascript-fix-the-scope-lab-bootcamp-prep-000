f693aa08d929c3ca4b067e314385bdcff95bb352
Lorenzo Covarrubias Jr <lorenzocovarrubiasjr@gmail.com>
2017-02-16 17:47:15 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-
 var animal = 'dog';
 
 function myAnimal() {
@@ -19,12 +18,12 @@ function add2(n) {
 
 
 
-var theFunk = funkyFunction();
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
   }
 }
+
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-
+var theFunk = funkyFunction()()
