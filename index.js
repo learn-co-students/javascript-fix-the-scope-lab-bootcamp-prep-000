e75b3546c2d13c9505bcab8998cda00397394817
Sarah Sweat <sarahsweat@gmail.com>
2017-03-07 22:23:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,24 +1,26 @@
 var animal = 'dog'
 
-function myAnimal() {
+function myAnimal()
+{
   return animal
 }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function yourAnimal()
+{
+  //redefining animal within scope of function
+  animal = 'cat'
   return animal
 }
 
-function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
+function add2(n)
+{
+  // must define two before using it in calculation
   const two = 2
+  return n + two
 }
 
-var funkyFunction = function() {
+var funkyFunction = function()
+{
   return function() {
     return "FUNKY!"
   }
@@ -26,4 +28,6 @@ var funkyFunction = function() {
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var theFunk = funkyFunction
+// I added two sets of parenthesis in order to execute both levels of functions.
+
+var theFunk = funkyFunction()()
