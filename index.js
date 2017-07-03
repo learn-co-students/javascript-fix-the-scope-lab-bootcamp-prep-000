6f4f89ee3c548212579fd43c397b4b328e1d7fe6
Emily Morgan <emorgan05@gmail.com>
2017-07-03 18:01:10 +0000

    Done.

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  var animal = cat
+  var animal = "cat"
   return animal
 }
 
@@ -27,4 +27,4 @@ var funkyFunction = function() {
 
 // We want 'funkyFunction' on the line below to return a function that returns "FUNKY!" -- how can we accomplish that?
 // NOTE: To pass this final test, you only need to modify the code below this line.
-var theFunk = funkFunction(function())
+var theFunk = funkyFunction()()
