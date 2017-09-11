d4e8f022432787ebe8579b48debc1515d7551709
Jeni Williams <stanfordjeni@gmail.com>
2017-09-11 19:04:59 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,14 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  return n + two
+  // Feel free to move things around!
 }
 
 var funkyFunction = function() {
