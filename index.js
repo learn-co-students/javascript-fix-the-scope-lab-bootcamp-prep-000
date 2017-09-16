be487965cc5d7de8e530e25967734dc11a10ec32
amckean12 <amckean@me.com>
2017-09-16 18:08:49 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
 var animal = 'dog'
 
+//Function will return dog
 function myAnimal() {
   return animal
 }
-
+//Function will return the local variable cat
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat';
   return animal
 }
 
+//Function defines two as 2 and defines answer as the number + two
 function add2(n) {
-  return n + two
+  var two = 2
+  var answer = n + two
+  return answer
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
@@ -26,4 +31,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+//Returns the string located in the second function of funkyFunction
+var theFunk = funkyFunction()()
