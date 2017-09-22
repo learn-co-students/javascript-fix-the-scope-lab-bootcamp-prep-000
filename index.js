428ac12fc05298406ae9a905f644a995680ef100
Brandon Chin <brandonchin@me.com>
2017-09-22 21:38:20 +0000

    Done.

--- index.js
+++ index.js
@@ -13,10 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
+const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = '2'
+
 }
 
 var funkyFunction = function() {
@@ -27,4 +28,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()();
