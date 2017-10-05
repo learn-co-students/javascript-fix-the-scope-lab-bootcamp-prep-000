0e77442d0937a706a489c7707b29fd0a35ac508c
Cheapo587 <ac.shultz587@gmail.com>
2017-10-05 23:01:46 +0000

    Done.

--- index.js
+++ index.js
@@ -7,15 +7,22 @@ function myAnimal() {
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  // P.S.: You can't just hard-code 'cat' below'
+
+  //solution: create a shadow variable animal assigned to the string cat
+  var animal = "cat"
+
   return animal
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
+  //solution: move the const variable declaration before the return statement
+  //const two = 2; declaration was originally here, and anything after the semicolon was added
 }
 
 var funkyFunction = function() {
@@ -26,4 +33,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction();
+theFunk = theFunk();
