324e7ebc414756112ebec6f55eed42952acfc459
Aubrey Neimeier <neimeier.1@osu.edu>
2017-10-21 16:41:21 +0000

    Old code that is not working

--- index.js
+++ index.js
@@ -1,15 +1,10 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
-}
-
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
-}
+  var animal = "cat"
+  return function yourAnimal(){
+    return animal
+  }
 
 function add2(n) {
   return n + two
