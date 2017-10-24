2bcbcb2ed61d44653c69d538b8a529c6000b24b6
Max Zelinka <fourthwalljoke@gmail.com>
2017-10-24 16:13:53 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+var animal = 'dog'
 
+function myAnimal() {
+  return animal
+}
+
+function yourAnimal() {
+  var animal = 'cat';
+  // How can we make sure that this function
+  // and the above function both pass?
+  // P.S.: You can't just hard-code 'cat' below
+  return animal
+}
+
+function add2(n) {
+  // Feel free to move things around!
+  const two = 2;
+  return n + two
+}
+
+var funkyFunction = function() {
+  return function() {
+    return 'FUNKY!';
+  }
+}
+var theFunk = funkyFunction()();
