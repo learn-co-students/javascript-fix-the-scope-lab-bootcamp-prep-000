6e5dedd51ea16444c3d68460f24725dc8ca49f0f
Marvin Andrade <marvin@lionguardsecurity.com>
2017-11-02 01:14:23 +0000

    Done.

--- index.js
+++ index.js
@@ -23,11 +23,12 @@ const two = 2
 
 var funkyFunction = function() {
   return  function() {
-    return "FUNKY!"
+    return 'FUNKY!'
   }
 
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // : you only need to modify the code below this line.
-var theFunk = funkyFunction()
+
+var theFunk =  funkyFunction()()
