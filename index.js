f0fcb6fb4fe534679b1a21fc4f6fd2f81d78bcd4
Eric Rogers <eric.rog17@gmail.com>
2017-11-08 02:12:07 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  // ANSWER: WE "SHADOW" THE GLOBAL VARIABLE
+  var animal = 'cat'
   return animal
 }
 
 function add2(n) {
+  // ANSWER: CONST TWO NEEDS TO BE DECLARED HERE AT THE TOP
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  // NOT DOWN HERE
 }
 
 var funkyFunction = function() {
@@ -26,4 +30,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
