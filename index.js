152db9ca3e12b7c2547fd0b4ec4332fb048d7fc6
Bethany  Walker <bethanymarciawalker@outlook.com>
2017-12-01 00:29:46 +0000

    Done.

--- index.js
+++ index.js
@@ -19,6 +19,7 @@ function add2(n) {
   // Feel free to move things around!
   }
 
+
 var funkyFunction = function() {
   return function() {
     return "FUNKY!"
@@ -27,5 +28,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-funkyFunction();
-
+var theFunk = funkyFunction()();
