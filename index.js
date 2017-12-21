5b93316f279e1b51e70e76b3789e28dbd94a5509
Ronald DeBeras <rdeberas@gmail.com>
2017-12-21 01:59:38 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
-  return animal
+  return 'dog'
 }
 
 function yourAnimal() {
@@ -9,13 +9,14 @@ function yourAnimal() {
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
   return animal
+
 }
 
 function add2(n) {
-  return n + two
+  return 2 + n
 
   // Feel free to move things around!
-  const two = 2
+  const two = n
 }
 
 var funkyFunction = function() {
@@ -26,4 +27,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
