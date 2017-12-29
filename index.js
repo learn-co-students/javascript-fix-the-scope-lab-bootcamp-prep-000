9214df9a617b0ff87a45d1273c92c20906b670be
kevin le <17786629@student.westernsydney.edu.au>
2017-12-29 13:09:56 +0000

    Done.

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  var animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,10 +13,11 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+
 }
 
 var funkyFunction = function() {
