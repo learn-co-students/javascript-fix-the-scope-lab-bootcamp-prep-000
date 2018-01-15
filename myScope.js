d0cf3d71f183acfc1e7218f4e130c73140209889
BRAULIO R URBINA <braulio_urbina@hotmail.com>
2018-01-15 19:45:30 +0000

    Done.

---
+++ myScope.js
@@ -0,0 +1,13 @@
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+
+var myScope = outerFunction()
+
+console.log(myScope());
