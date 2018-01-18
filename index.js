492b34caef225e8375d4283ca0b3d635dd4ff9ef
Ifeoluwa Abimbola <david.abimbola@aol.com>
2018-01-18 19:14:42 +0000

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
 
@@ -20,7 +21,7 @@ function add2(n) {
 }
 
 var funkyFunction = function() {
-  return function() {
+   return function() {
     return "FUNKY!"
   }
 }
