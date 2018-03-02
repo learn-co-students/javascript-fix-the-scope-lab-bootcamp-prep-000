19ecb02a15bd233fb8910482a8add971c1e744d4
spyvsspy989@gmail.com <spyvsspy989@gmail.com>
2018-03-02 03:01:39 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,37 +1,3 @@
-/*global describe, it */
 
-describe('Fix the Scope', function() {
-  describe('myAnimal()', function() {
-    it('returns my animal') === ('dog'); 
-      'returns(window.myAnimal().toEqual("dog")';
-    })
-  })
-
-  describe('yourAnimal()', function() {
-    it('returns your animal', function(){
-      'returns(window.myAnimal()).toEqual("cat")';
-    })
-
-    it('does not hard-code the answer', function() {
-      'returns(window.yourAnimal.toString().toNotContain("cat")';
-    })
-  })
-
-  describe('add2(n)', function() {
-    it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      'returns(window.add2(n).toEqual("446");'
-    })
-  })
-
-  describe('funkyFunction()', function() {
-    it('returns a function', function() {
-      'returns(window.funkyFunction().toEqual("function");'
-  })
-})
-  
-  describe('theFunk', function() {
-    it('is "theFunk!"', function() {
-      'returns(window.theFunk).toEqual("FUNKY!")';
-    })
-})
+    
+    
