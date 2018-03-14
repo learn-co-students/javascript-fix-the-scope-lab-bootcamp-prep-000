abacd9dce8fa13954d94b2a838bfd56027945176
Daila Viteri <dailaviteri@gmail.com>
2018-03-14 00:32:06 +0000

    Old code that is not working

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  var animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,17 +13,13 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return n + 2
 
   // Feel free to move things around!
-  const two = 2
-}
+  const two = n }
 
-var funkyFunction = function() {
-  return function() {
+var theFunk="Funky!"
     return "FUNKY!"
-  }
-}
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
