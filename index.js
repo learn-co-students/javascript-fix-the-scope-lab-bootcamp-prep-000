83a1dae357297e4a3fd5ae4c383f8b5cd2ed3c04
Jciv.arch@gmail.com <jciv.arch@gmail.com>
2018-04-01 18:41:18 +0000

    Done.

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+  var animal = "cat"
   return animal
 }
 
