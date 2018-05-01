731e3a1ce7ab1ae212b7882fdd4643f639be36bc
drewisatlas@gmail.com <drewisatlas@gmail.com>
2018-05-01 00:00:34 +0000

    Done.

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 
-console.log(funkyFunction)
 
-var theFunk = funkyFunction()
+var theFunk = funkyFunction
 
-console.log(theFunk)
+console.log(theFunk()())
