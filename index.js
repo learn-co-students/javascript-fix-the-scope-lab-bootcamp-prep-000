f2d7ba70510c0da2b3416459eb27f122eed62fd2
Steven Crouse <sdcrouse91@gmail.com>
2018-05-07 00:29:52 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,36 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
+  return animal;
 }
 
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  return animal
+  var differentAnimal = 'cat';
+  return differentAnimal;
 }
 
 function add2(n) {
-  return n + two
+  
+  const two = 2;
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+var tooFunky = funkyFunction();
+
+var theFunk = tooFunky();
+
+/* So, here's how this works: First, call funkyFunction, which returns its inner function as its value. Then, by setting the variable tooFunky equal to that call to funkyFunction, tooFunky and funkyFunction's inner function are now one and the same. So, to get the string "FUNKY!" from that inner function, just call tooFunky, and set its return value ("FUNKY!") equal to theFunk. */
