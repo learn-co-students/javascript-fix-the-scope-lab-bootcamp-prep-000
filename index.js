573a912276e43fe0e9453e2331ee219eeb3eeeab
Chantal Wallace <theteatreehouse@gmail.com>
2018-05-08 01:23:00 +0000

    Done.

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
+  const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
