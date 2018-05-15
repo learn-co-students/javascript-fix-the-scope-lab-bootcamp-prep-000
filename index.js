a729f00e610105b6a5acc9ca5929941058f72d00
Cam Holloway <hollowprince@outlook.com>
2018-05-15 19:23:20 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 var animal = 'dog'
+const two = 2
 
 function myAnimal() {
   return animal
@@ -8,6 +9,7 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat'
   return animal
 }
 
@@ -15,7 +17,7 @@ function add2(n) {
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
@@ -26,4 +28,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = "FUNKY!"
+// the hints are too vague and the lesson prior to this does not properly prepare users for being able to execute a function from within a function. Only how to call a variable from within a function, which doesn't work at all in reverse. Even the most intense googling doesn't result in an answer that works in the designated line of code
