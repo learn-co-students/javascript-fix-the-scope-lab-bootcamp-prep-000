cb4ffd26bb2ed51b19a8cff76d848796acbeaacc
Princess Shuri <webdevdeja@gmail.com>
2018-05-22 19:39:35 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -3,37 +3,37 @@
 describe('Fix the Scope', function() {
   describe('myAnimal()', function() {
     it('returns my animal', () => {
-      expect(window.myAnimal()).toEqual('dog')
-    })
-  })
+      expect(window.myAnimal()).toEqual('dog');
+    });
+  });
 
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
-      expect(window.yourAnimal()).toEqual('cat')
-    })
+      expect(window.yourAnimal()).toEqual('cat');
+    });
 
     it('does not hard-code the answer', function() {
-      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
-    })
-  })
+      expect(window.yourAnimal.toString()).toNotContain("return 'cat'");
+    });
+  });
 
   describe('add2(n)', function() {
     it('adds two to n', function() {
-      const n = Math.floor(Math.random() * 1000)
-      expect(window.add2(n)).toEqual(n + 2)
-    })
-  })
+      const n = Math.floor(Math.random() * 1000);
+      expect(window.add2(n)).toEqual(n + 2);
+    });
+  });
 
   describe('funkyFunction()', function() {
     it('returns a function', function() {
-      expect(typeof window.funkyFunction()).toEqual('function')
-    })
-  })
+      expect(typeof window.funkyFunction()).toEqual('function');
+    });
+  });
 
   describe('theFunk', function() {
     it('is "FUNKY!"', function() {
-      expect(window.theFunk).toEqual('FUNKY!')
-    })
-  })
+      expect(window.theFunk).toEqual('FUNKY!');
+    });
+  });
 
-})
+});
