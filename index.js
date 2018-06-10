46ebd74bc796e6af4f4b9dc3a997fec344494d70
Voumo Tiogo Joel Brice <tiogoj@hotmail.com>
2018-06-10 19:20:37 +0000

    Done.

--- index.js
+++ index.js
@@ -19,12 +19,14 @@ function add2(n) {
   // Feel free to move things around!
 }
 
-var funkyFunction = function() {
-  return function() {
+var funkyFunction = function(){
+  return function(){
     return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE:you only need to modify the code below this line.
-var theFunk = funkyFunction()
+/* note you only need to modify the code below this line.*/
+
+var theFunk = funkyFunction()();
+theFunk();
