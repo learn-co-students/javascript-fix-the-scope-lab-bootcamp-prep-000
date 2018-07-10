df518a067bd7b35dd192c8946eb9d2ef9fb95b00
mssigler@gmail.com <mssigler@gmail.com>
2018-07-10 00:44:16 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,37 @@
 var animal = 'dog'
-
-function myAnimal() {
+function myAnimal() {	
   return animal
 }
 
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
+function yourAnimal() {				
+  var animal = 'cat'
   return animal
 }
 
-function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+function add2(n) {					
+ const two = 2  
+ return n + two
 }
 
-var funkyFunction = function() {
+var funkyFunction = function() {		//  Got [Function: funkyFunction]. Should get: ’FUNKY!'
   return function() {
     return "FUNKY!"
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction();
+theFunk();
+
+var funkyFunction = function() {	
+  var innerVar =  "FUNKY!"
+  return function() {
+  return innerVar;
+  }
+}
+var theFunk = funkyFunction();
+
+theFunk();
+
+//both of these funkyFunctions 
+//return "FUNKY!" in Sublimetext. 
+//I'm not sure what you people are looking for. 
