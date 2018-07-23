9584e42f4aefa184937a63eb8a2a8b4a9dd83a2f
Jon Mackey <jon@jonmackey.party>
2018-07-23 20:14:28 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,11 +1,13 @@
 /*global describe, it */
 
+
 describe('Fix the Scope', function() {
   describe('myAnimal()', function() {
     it('returns my animal', () => {
       expect(window.myAnimal()).toEqual('dog')
     })
   })
+
 
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
@@ -17,12 +19,14 @@ describe('Fix the Scope', function() {
     })
   })
 
+
   describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
     })
   })
+
 
   describe('funkyFunction()', function() {
     it('returns a function', function() {
