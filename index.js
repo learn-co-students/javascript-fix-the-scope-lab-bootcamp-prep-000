75efebed33ed22a89725d3572e94b9a41b2e36cc
Susan Lockwood <susan.m.mullin@gmail.com>
2018-08-02 03:28:16 +0000

    Done.

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ function myAnimal() {
   return animal
 }
 
+function myAnimal() {
+var animal = 'dog';
+return animal;
+}
+
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
@@ -11,11 +16,21 @@ function yourAnimal() {
   return animal
 }
 
+function yourAnimal() {
+  var animal = 'cat';
+  return animal;
+}
+
 function add2(n) {
   return n + two
 
   // Feel free to move things around!
   const two = 2
+}
+
+function add2(n) {
+ var two = 2
+  return n + two;
 }
 
 var funkyFunction = function() {
@@ -27,3 +42,4 @@ var funkyFunction = function() {
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction
+var theFunk = funkyFunction()()
--- test/index-test.js
+++ test/index-test.js
@@ -7,6 +7,11 @@ describe('Fix the Scope', function() {
     })
   })
 
+    function myAnimal() {
+    var animal = 'dog';
+    return animal;
+  }
+
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
       expect(window.yourAnimal()).toEqual('cat')
@@ -17,23 +22,46 @@ describe('Fix the Scope', function() {
     })
   })
 
+  function yourAnimal() {
+    var animal = 'cat';
+    return animal;
+  }
+
   describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
       expect(window.add2(n)).toEqual(n + 2)
     })
   })
+  
+    function add2(n) {
+   var two = 2
+    return n + two;
+  }
 
   describe('funkyFunction()', function() {
     it('returns a function', function() {
       expect(typeof window.funkyFunction()).toEqual('function')
     })
   })
+  
+    var funkyFunction = function() {
+    return function(){
+      return "FUNKY!"
+    }
+}
 
   describe('theFunk', function() {
     it('is "FUNKY!"', function() {
       expect(window.theFunk).toEqual('FUNKY!')
     })
   })
+
+  var funkyFunction = function() {
+    return function(){
+      return "FUNKY!"
+    }
+  }
+   var theFunk = funkyFunction()()
 
 })
