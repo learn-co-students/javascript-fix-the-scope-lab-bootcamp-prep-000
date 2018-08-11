78db3f0b887e6ff8a6effd52f9269a7a5f956a87
rayzafir@aol.com <rayzafir@aol.com>
2018-08-11 14:26:35 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var animal = 'dog'
+var animal = 'cat'
 
 function myAnimal() {
   return animal
@@ -12,7 +12,7 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
+  return (n + 2)
 
   // Feel free to move things around!
   const two = 2
