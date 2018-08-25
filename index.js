e2fee1d9c3ea742ca4334c8d1b4cbd7344b47d6a
Cedric David <cedric.david@btopenworld.com>
2018-08-25 10:37:23 +0000

    Done.

--- index.js
+++ index.js
@@ -6,15 +6,15 @@ function myAnimal() {
 }
 
 function yourAnimal() {
+  animal = 'cat'
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
-  animal = "cat";
   return animal
 }
 
 function add2(n) {
-const two = 2;
+  const two = 2
   return n + two
 
   // Feel free to move things around!
@@ -27,6 +27,7 @@ var funkyFunction = function() {
   }
 }
 
-// We want this to return a function that returns "FUNKY!" -- how can we accomplish that?
-var theFunk = funkyFunction();
-theFunk = theFunk();
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+
+// NOTE: you only need to modify the code below this line.
+var theFunk = funkyFunction()();
