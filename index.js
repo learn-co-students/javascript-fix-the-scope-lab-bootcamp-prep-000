3194f7417df796036fcb952c3f8cd8993d9a17a6
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-09-09 21:13:34 +0000

    Done.

--- index.js
+++ index.js
@@ -1,29 +1,1 @@
-var animal = 'cat'
-
-function myAnimal() {
-  return animal
-}
-
-function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
-}
-
-function add2(n) {
-  return n + 2
-
-  // Feel free to move things around!
-  const two = 2;
-}
-
-var funkyFunction = function() {
-  return function() {
-    return 'FUNKY!'
-  }
-}
-
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction()();
+var animal = 'dog'function myAnimal() {  return animal}function yourAnimal() {  // How can we make sure that this function  // and the above function both pass?  var animal = 'cat'  // P.S.: You can't just hard-code 'cat' below  return animal}function add2(n) {  const two = 2  return n + two  // Feel free to move things around!}var funkyFunction = function () {  return function () {    return 'FUNKY!'  }}// We want to set theFunk equal to "FUNKY!" using our funkyFunction.// NOTE: you only need to modify the code below this line.var theFunk = funkyFunction()();
