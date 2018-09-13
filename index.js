eb95cbbd24458de61b1be3810e156db685c98906
Jeff Carter <jeff@pepperfly.com>
2018-09-13 20:43:11 +0000

    Done.

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 var animal = 'dog'
+const two = 2;
+var n;
 
 function myAnimal() {
   return animal
@@ -8,22 +10,27 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  var animal = 'cat';
   return animal
 }
 
 function add2(n) {
-  return n + two
+  return n + two;
 
   // Feel free to move things around!
-  const two = 2
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return "FUNKY!";
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+// Jeff says:
+// This works in other code sandboxes. Why am I still getting an error?
+var theFunk = funkyFunction();
+
+//
