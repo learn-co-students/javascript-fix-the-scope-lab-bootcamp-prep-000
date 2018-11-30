a5675a90ad10f4dddca17a4e8f907a96b8e80237
Paris Dolfman <parisdolfman@gmail.com>
2018-11-30 17:07:46 +0000

    Done.

--- index.js
+++ index.js
@@ -14,10 +14,10 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
 
   // Feel free to move things around!
   const two = 2
+  return n + two
 }
 
 var funkyFunction = function() {
