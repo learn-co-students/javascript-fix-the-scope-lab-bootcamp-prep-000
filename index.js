5afee60a828018f64e5d62eab70641a9d78ea4bb
rnavedojr <rnavedojr@outlook.com>
2018-12-25 19:13:29 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,19 @@
-var animal = 'dog'
+var animal = 'dog';
+myAnimal();
 
 function myAnimal() {
-  return animal
+  animal = 'dog';
+  return animal;
 }
 
 function yourAnimal() {
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: You can't just hard-code 'cat' below
-  return animal
+  animal = 'cat';
+  return animal;
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
-  const two = 2
+  const two = 2;
+  return n + two;
 }
 
 var funkyFunction = function() {
