3fdfee925a7bb6cb67091e5ff78fc1101ca6b49d
Ben Manekin <ben.manekin@gmail.com>
2019-01-10 15:47:24 +0000

    Done.

---
+++ junk.js
@@ -0,0 +1,15 @@
+function outerFunction() {
+  var innerVariable = "I'm sort of a secret.";
+ 
+  return function innerScope() {
+    var inaccessible = "Nothing can touch me.";
+ 
+    return innerVariable;
+  }
+}
+
+var myScope = outerFunction();
+
+console.log(myScope());
+console.log(myScope);
+console.log(outerFunction());
