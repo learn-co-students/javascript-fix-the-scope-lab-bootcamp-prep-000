1b3993020b90914d42d6b9bc69c9e71d130a007b
Erini George <dr.erinigeorge@yahoo.com>
2019-01-30 17:31:21 +0000

    Done.

--- index.js
+++ index.js
@@ -29,8 +29,10 @@ function jojo() {
   return koko()
 }
   
- console.log(jojo());
+// console.log(jojo());
 var funkyFunction = jojo()
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
 var theFunk = funkyFunction;
+
+//console.log(theFunk);
