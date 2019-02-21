300cbf06fdeba77867b966a815cbb252f2d04ea0
remi mendoza <mendozaremi@gmail.com>
2019-02-21 02:24:14 +0000

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -5,11 +5,6 @@ describe('Fix the Scope', function() {
     })
   })
 
-    function myAnimal() {
-    var animal = 'dog';
-    return animal;
-  }
-
   describe('yourAnimal()', function() {
     it('returns your animal', function(){
       expect(window.yourAnimal()).toEqual('cat')
@@ -20,11 +15,6 @@ describe('Fix the Scope', function() {
     })
   })
 
-  function yourAnimal() {
-    var animal = 'cat';
-    return animal;
-  }
-
   describe('add2(n)', function() {
     it('adds two to n', function() {
       const n = Math.floor(Math.random() * 1000)
@@ -32,36 +22,16 @@ describe('Fix the Scope', function() {
     })
   })
 
-  function add2(n) {
-   var two = 2
-    return n + two;
-  }
-
   describe('funkyFunction()', function() {
     it('returns a function', function() {
       expect(typeof window.funkyFunction()).toEqual('function')
     })
   })
 
-  var funkyFunction = function() {
-    return function(){
-      return "FUNKY!"
-    }
-}
-
-
   describe('theFunk', function() {
     it('is "FUNKY!"', function() {
       expect(window.theFunk).toEqual('FUNKY!')
     })
   })
-
-  var funkyFunction = function() {
-    return function(){
-      return "FUNKY!"
-    }
-  }
-
-  var theFunk = funkyFunction()()
 
 })
