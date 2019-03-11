93ec41e780d96f42d9f164833d6ad2acaa46a370
Ethan Freire <element55star@gmail.com>
2019-03-11 14:26:54 +0000

    Done.

--- index.js
+++ index.js
@@ -8,7 +8,9 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+   var animal
   return animal
+
 }
 
 function add2(n) {
