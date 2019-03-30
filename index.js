10045df3ae1c4d3652b82638f26d2d63091ad8df
James Merkin <jnmerk@gmail.com>
2019-03-30 16:46:59 +0000

    Done.

--- index.js
+++ index.js
@@ -1,21 +1,23 @@
 var animal = 'dog'
 
 function myAnimal() {
-  return animal
+    return animal
 }
 
-function yourAnimal() {
+function yourAnimal(animal) {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal='cat'
   return animal
 }
 
 function add2(n) {
+var two=2;
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+  
 }
 
 var funkyFunction = function() {
@@ -26,4 +28,12 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+
+var temp;
+var theFunk;
+temp=funkyFunction();
+theFunk=temp();
+
+
+//https://learn.co/tracks/bootcamp-prep/javascript-fundamentals/functions-and-scope/fix-the-scope-lab#
