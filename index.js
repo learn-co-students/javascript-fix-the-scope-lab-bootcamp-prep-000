202ca45cd6e93d524ef8257f733e0a6028648c2a
Xue(Yuki) Yao <ukiyao@hotmail.com>
2019-04-29 03:30:26 +0000

    Done.

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-var animal = 'dog'
+var animal = 'dog' //<-- global
 
 function myAnimal() {
-  return animal
+  return animal //<-- global 
 }
 
 function yourAnimal() {
+  var animal = 'cat' //<-- declare local varialble animal
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
@@ -12,18 +13,24 @@ function yourAnimal() {
 }
 
 function add2(n) {
-  return n + two
-
-  // Feel free to move things around!
   const two = 2
+  //declared varialble in block scope
+  return (n + two)
+  // Feel free to move things around!
 }
 
 var funkyFunction = function() {
   return function() {
-    return "FUNKY!"
+    return 'FUNKY!'
   }
 }
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+
+var temp = funkyFunction()
+//Temp now is the returned anonymous function name inside funkyFunction, the stringified version of `anonymous()`
+//it enabled funkyFunction's argu, but not anonymous func's argu;
+
+var theFunk = temp();
+// call temp function; it will return "FUNKY!"
