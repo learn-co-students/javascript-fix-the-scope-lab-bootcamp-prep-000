75e3b0f2065c3e9aec6406c91bdc6b39887717f9
Abdullah Bajaman <ambajaman@gmail.com>
2019-06-09 09:50:48 +0000

    Done.

--- index.js
+++ index.js
@@ -4,19 +4,48 @@ function myAnimal() {
   return animal
 }
 
+/***********************************************************************************
+ * describe('yourAnimal()', function() {
+    it('returns your animal', function(){
+      expect(window.yourAnimal()).toEqual('cat')
+    })
+
+    it('does not hard-code the answer', function() {
+      expect(window.yourAnimal.toString()).toNotContain("return 'cat'")
+    })
+  })
+  */
 function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal='cat'
   return animal
 }
 
 function add2(n) {
-  return n + two
+  
 
   // Feel free to move things around!
   const two = 2
+  return n + two
 }
+
+/************************************************************************************
+ *  describe('funkyFunction()', function() {
+    it('returns a function', function() {
+      expect(typeof window.funkyFunction()).toEqual('function')
+    })
+  })
+
+  describe('theFunk', function() {
+    it('is "FUNKY!"', function() {
+      expect(window.theFunk).toEqual('FUNKY!')
+    })
+  })
+
+})
+*/
 
 var funkyFunction = function() {
   return function() {
@@ -26,4 +55,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
+
