171be734b5a337c51818dd06a32b45f67262aaf5
decent-port-4955 <emil@georgetownoliveoil.com>
2019-09-18 03:58:18 +0000

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var animal = 'dog'
 
 function myAnimal() {
+  var animal = "dog"
   return animal
+  
 }
 
 function yourAnimal() {
@@ -10,13 +12,21 @@ function yourAnimal() {
   // P.S.: You can't just hard-code 'cat' below
   return animal
 }
+function yourAnimal() {
+  var animal = 'cat';
+  return animal;
+}
 
 function add2(n) {
+  var two = 2
   return n + two
 
+
   // Feel free to move things around!
-  const two = 2
+ 
 }
+
+
 
 var funkyFunction = function() {
   return function() {
@@ -26,4 +36,4 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
