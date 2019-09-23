34c950ee142bcf3698b5f2413396d74c40a9ba41
Kien Ngo <kienngo82@gmail.com>
2019-09-23 07:23:29 +0000

    Done.

--- index.js
+++ index.js
@@ -8,14 +8,16 @@ function yourAnimal() {
   // How can we make sure that this function
   // and the above function both pass?
   // P.S.: You can't just hard-code 'cat' below
+  animal = 'cat'
   return animal
 }
 
 function add2(n) {
+   const two = 2
   return n + two
 
   // Feel free to move things around!
-  const two = 2
+ 
 }
 
 var funkyFunction = function() {
@@ -26,4 +28,5 @@ var funkyFunction = function() {
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+//Never in a million would I have figure this one one out. YOu call a function within a function from () adding another (). Therefore ()() is your answer here.
+var theFunk = funkyFunction()();
