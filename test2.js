9d8828a67e25ff5b356c659db815a77c4b8364ae
rajmehta54 <rajmehta54@gmail.com>
2019-10-22 16:42:57 +0000

    Done.

---
+++ test2.js
@@ -0,0 +1,12 @@
+var funkyFunction = function() {
+  return function() {
+    return "FUNKY!"
+  }
+}
+
+// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
+// NOTE: you only need to modify the code below this line.
+funkyFunction()
+var theFunk = funkyFunction
+theFunk()
+return theFunk
