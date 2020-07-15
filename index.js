8140fe51d172a365f198db9255929cb743411962
Rahel Hagos <hagos.rahelh@gmail.com>
2020-07-15 02:30:25 +0000

    Done.

--- index.js
+++ index.js
@@ -38,9 +38,9 @@ var funkyFunction = function()
   }
   
 }
-theFunk ="FUNKY!"
-var theFunk = funkyFunction
 
+var theFunk = funkyFunction
+theFunk ="FUNKY!"
 
 // We want to set theFunk equal to "FUNKY!" using our funkyFunction.
 // NOTE: you only need to modify the code below this line.
