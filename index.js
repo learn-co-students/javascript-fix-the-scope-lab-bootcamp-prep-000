f6cfe14a094f82d2cfc50dcfd9eb5673768824aa
scrappy-cyborg-4243 <itchkimel@gmail.com>
2020-07-16 20:03:38 +0000

    Done.

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
+  var animal = "cat"
   return animal
 }
-
-function add2(n) {
+function add2(n){
+  const two = 2 
   return n + two
+}
 
-  // Feel free to move things around!
-  const two = 2
+//technicaly, below is the ideal way
+function add2(n) {
+  return n +=2
 }
 
 var funkyFunction = function() {
@@ -27,6 +25,6 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
+
+    
