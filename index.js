627f95f75f183d8becdd0eec980628f5eac8b984
dauntless-strategy-1036 <danielgordon19972@gmail.com>
2020-11-05 09:41:30 +0000

    Done.

--- index.js
+++ index.js
@@ -6,20 +6,17 @@ function myAnimal() {
 }
 
 function yourAnimal() {
-  // The tests expect this function to return `animal` just like the previous function
-  // However, you cannot simply modify the existing variable declared on line 1 in the global scope 
-  // How can we make sure that this function
-  // and the above function both pass?
-  // P.S.: Hard-coding 'cat' below will not work
+  var animal
+  animal = "cat"
   return animal
 }
 
 function add2(n) {
-  return n + two
+  return n + 2
 
-  // Feel free to move things around!
-  const two = 2
+  // Feel free to move things around
 }
+
 
 var funkyFunction = function() {
   return function() {
@@ -27,6 +24,4 @@ var funkyFunction = function() {
   }
 }
 
-// We want to set theFunk equal to "FUNKY!" using our funkyFunction.
-// NOTE: you only need to modify the code below this line.
-var theFunk = funkyFunction
+var theFunk = funkyFunction()()
